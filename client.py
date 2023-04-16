c7b4ff441a9c5d5778b7fe2d92eed93044a86b31
ElisDiep <116689861+ElisDiep@users.noreply.github.com>
2023-04-15 15:05:31 -1000

    Update client.py

--- client.py
+++ client.py
@@ -5,22 +5,32 @@ import time
 #file size declared
 BUFFER_SIZE = 4096
 #move to the directory where images are
-os.chdir('imagewithgeotag4')
-
+os.chdir('image')
 #host of the GCS
 HOST = '192.168.137.1'
 #port number can be anything but small numbers since the ports are occupied
 PORT = 5000
-filenames = ['image1.jpg','image3.jpg','image6.jpg','image9.jpg','image12.jpg','image18.jpg','image20.jpg','image1.jpg_original']
-num_of_images = len(filenames)
+#number of images to be taken
+num_of_images = 20
 
+filenames = []
+folderName = "image"
 for x in range(num_of_images):
+	filenames.append(f"image{x+1}.jpg")
+	
+filenames.append("image1.jpg_original")
+#numbers of the array
+num_of_files = len(filenames)
+#transfer the images to GCS
+for x in range(num_of_files):
+	#waiting for images to be in the folder before transferring
 	while True:
-		path = (f"/home/UHDT_Pi/DataTransfer/imagewithgeotag4/"+filenames[x])
+		path = (f"/home/UHDT_Pi/DataTransfer/{folderName}/"+filenames[x])
 		if(os.path.isfile(path)):
 			break
-		print("FILE NOT FOUND")
+		print(f"FILE {filename[x]} NOT FOUND")
 		time.sleep(1)
+	#creating a connection
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print(f"[+] Connecting to {HOST}:{PORT}")
 	client.connect((HOST,PORT))
